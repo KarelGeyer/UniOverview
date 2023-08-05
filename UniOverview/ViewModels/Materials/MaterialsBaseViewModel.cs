18d2ab9887f1cd79b737ea3ad1bc2ea880b79075
Karel Geyer <karelgeyer@gmail.com>
2023-08-05 19:06:58 +0200

    Add some basic view models

---
+++ UniOverview/ViewModels/Materials/MaterialsBaseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniOverview.ViewModels.Materials
+{
+    public class MaterialsBaseViewModel : ViewModelBase { }
+}
