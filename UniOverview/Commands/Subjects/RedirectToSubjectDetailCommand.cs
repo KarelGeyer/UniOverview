2ecccf037d760d0274f332e936bb2dee6ef2b4a5
Karel Geyer <karelgeyer@gmail.com>
2023-08-06 22:15:58 +0200

    Prepare Command to handle logic behind navigating to a detail view for single subject

---
+++ UniOverview/Commands/Subjects/RedirectToSubjectDetailCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UniOverview.Models.Subjects;
+using UniOverview.Services;
+using UniOverview.ViewModels.Subjects;
+
+namespace UniOverview.Commands.Subjects
+{
+	public class RedirectToSubjectDetailCommand : CommandBase
+	{
+		private readonly CommandBase NavigationCommand;
+		private readonly SubjectsBaseViewModel SubjectsBaseViewModel;
+		private readonly SubjectViewModel SubjectViewModel;
+
+		public RedirectToSubjectDetailCommand(
+			CommandBase _navigationCommand,
+			SubjectsBaseViewModel _subjectsBaseViewModel,
+			SubjectViewModel _subjectViewModel
+		)
+		{
+			NavigationCommand = _navigationCommand;
+			SubjectsBaseViewModel = _subjectsBaseViewModel;
+			SubjectViewModel = _subjectViewModel;
+		}
+
+		public override void Execute(object? parameter)
+		{
+			if (parameter is null || parameter is not string)
+				throw new ArgumentNullException(nameof(parameter));
+
+			Guid subjectId = Guid.Parse(parameter as string);
+			Subject currentSubject = SubjectsBaseViewModel.GetSelectedSubject(subjectId);
+			SubjectViewModel.CurrentSubject = currentSubject;
+
+			NavigationCommand.Execute(parameter);
+		}
+
+		private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			OnCanExecuteChanged();
+		}
+	}
+}
