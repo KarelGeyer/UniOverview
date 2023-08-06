69d80c2f49c64e964e3ee5ae1bd92669b048cb78
Karel Geyer <karelgeyer@gmail.com>
2023-08-06 22:17:25 +0200

    Create a basic converter to convert boolean values to colors to display different icon background color when subject is either failed or already succesfully finished.

---
+++ UniOverview/Helpers/Converters/BoolToColorConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace UniOverview.Helpers.Converters
+{
+	class BoolToColorConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is bool boolValue)
+			{
+				if (parameter.Equals("IsCompleted"))
+					return boolValue ? "#90EE90" : "#FFF";
+				if (parameter.Equals("IsFailed"))
+					return boolValue ? "#FF0000" : "#FFF";
+			}
+
+			return DependencyProperty.UnsetValue;
+		}
+
+		public object ConvertBack(
+			object value,
+			Type targetType,
+			object parameter,
+			CultureInfo culture
+		)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
