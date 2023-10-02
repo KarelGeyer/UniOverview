c9a04438a5a0c849fc8eeea1e5b2e2e761a24fc2
Karel Geyer <karelgeyer@gmail.com>
2023-10-02 23:06:56 +0200

    Add new model for teacher name for displaying purposes and move Teacher to a new Teacher folder

---
+++ UniOverview/Models/Teachers/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniOverview.Models.Teachers
+{
+	public class Teacher
+	{
+		public Guid Id { get; set; }
+		public required string Name { get; set; }
+		public required string Surname { get; set; }
+		public string? Email { get; set; }
+		public string? Title { get; set; }
+	}
+}
---
+++ UniOverview/Models/Teachers/TeachersName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniOverview.Models.Teachers
+{
+	public class TeachersName
+	{
+		public string? Name { get; set; }
+	}
+}
