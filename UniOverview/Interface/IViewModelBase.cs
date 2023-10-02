55c6371b8e79538aaba658982223a990df4e7543
Karel Geyer <karelgeyer@gmail.com>
2023-10-02 23:05:45 +0200

    Add new interface

---
+++ UniOverview/Interface/IViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniOverview.Interface
+{
+	public interface IViewModelBase
+	{
+		public void Dispose();
+		public void OnPropertyChanged(string propertyName);
+		public void OnLoad();
+	}
+}
