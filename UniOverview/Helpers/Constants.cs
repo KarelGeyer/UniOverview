69d1ea59a30261ebacaf4f9ce2cc9ff8155152e2
Karel Geyer <karelgeyer@gmail.com>
2023-10-02 23:05:08 +0200

    Add new helpers

---
+++ UniOverview/Helpers/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UniOverview.Helpers
+{
+	public static class Constants
+	{
+		public static Regex EmailRegex = new Regex(
+			"^[-a-zA-Z0-9~!$%^&*_=+}{\\'?]+(\\.[-a-zA-Z0-9~!$%^&*_=+}{\\'?]+)*@([a-zA-Z0-9_][-a-zA-Z0-9_]*(\\.[-a-zA-Z0-9_]+)*\\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-zA-Z]+)|([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}))(:[0-9]{1,5})?$"
+		);
+
+		public static Regex PhoneRegex = new Regex("^(\\+420|00420) ?[6-7][0-9][0-9] ?([0-9][0-9][0-9] ?[0-9][0-9][0-9]|[0-9][0-9] ?[0-9][0-9] ?[0-9][0-9])$");
+
+		public static Regex PasswordRegex = new Regex("^(?!.* )(?=.*\\d)(?=.*[a-zA-Z]).{8,}$");
+	}
+}
---
+++ UniOverview/Helpers/FormValidation.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using UniOverview.Enums.Subjects;
+using UniOverview.Models.Subjects;
+
+namespace UniOverview.Helpers
+{
+	public static class FormValidation
+	{
+		public static bool ValidateBasicString(string[] strings)
+		{
+			foreach (string s in strings)
+				return !string.IsNullOrEmpty(s);
+			return true;
+		}
+
+		public static bool ValidateBasicString(string value)
+		{
+			return !string.IsNullOrEmpty(value);
+		}
+
+		public static bool ValidateIntegersAndFloatsHigherThenZero(int[]? integers, float[]? floats)
+		{
+			if (integers == null && floats == null)
+				throw new ArgumentNullException(nameof(floats) + nameof(floats), "Všechny parametry jsou prázdné");
+			if (integers != null && integers.Length != 0)
+			{
+				foreach (int i in integers)
+					return i > 0;
+			}
+
+			if (floats != null && floats.Length != 0)
+			{
+				foreach (float f in floats)
+					return f > 0;
+			}
+
+			return true;
+		}
+
+		public static string ValidateEmail(string email)
+		{
+			if (string.IsNullOrEmpty(email))
+				throw new ArgumentNullException(nameof(email), "Prázdný email na vstupu.");
+
+			email = email.Replace(" ", "").Trim();
+			if (!Constants.EmailRegex.Match(email).Success)
+				throw new ArgumentNullException(nameof(email), "Nevalidní email na vstupu.");
+
+			return email;
+		}
+
+		public static CompletionDetails ValidateAddNewSubjectCompletionDetails(
+			string subjectType,
+			string subjectTypeOfCompletion,
+			string subjectFormOfCompletion
+		)
+		{
+			if (string.IsNullOrEmpty(subjectType) || string.IsNullOrEmpty(subjectTypeOfCompletion) || string.IsNullOrEmpty(subjectFormOfCompletion))
+				throw new ArgumentNullException(nameof(subjectType), "Jeden ze vstupů je prázdný");
+
+			SubjectType _subjectType;
+			TypeOfCompletion _typeOfCompletion;
+			FormOfCompletion _formOfCompletion;
+
+			Enum.TryParse(subjectType, out _subjectType);
+			Enum.TryParse(subjectTypeOfCompletion, out _typeOfCompletion);
+			Enum.TryParse(subjectFormOfCompletion, out _formOfCompletion);
+
+			return new CompletionDetails()
+			{
+				TypeOfSubject = _subjectType,
+				SubjectTypeOfCompletion = _typeOfCompletion,
+				SubjectFormOfCompletion = _formOfCompletion
+			};
+		}
+	}
+}
