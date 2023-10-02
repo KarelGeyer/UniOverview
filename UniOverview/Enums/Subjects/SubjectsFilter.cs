d5fcb4c758a16698a78dfc1b723388fa666b03ab
Karel Geyer <karelgeyer@gmail.com>
2023-10-02 23:05:35 +0200

    Add new enums

---
+++ UniOverview/Enums/Subjects/SubjectsFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniOverview.Enums.Subjects
+{
+	public enum SubjectsFilter
+	{
+		Alphabet,
+		Completed,
+		Failed,
+		SubjectType,
+	}
+}
